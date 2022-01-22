15291ce9cbfd414acec1b8c0cb317715ddcd8e11
Alex Coy <88282738+CaptainCoy@users.noreply.github.com>
2022-01-21 17:59:14 -0700

    commit

--- Django_Checkbook/Django_Checkbook/Checkbook/views.py
+++ Django_Checkbook/Django_Checkbook/Checkbook/views.py
@@ -7,6 +7,7 @@ def home(request):
     form = TransactionForm(data=request.POST or None)
     if request.method == 'POST':
         pk = request.POST['account']
+        return balance(request, pk)
     content = {'form': form}
     return render(request, 'checkbook/index.html', content)
 
